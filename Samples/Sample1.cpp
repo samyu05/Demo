55f200ea5d8a350f1cc704ffdc5bd8297484af22
samyu05 <sheelamsamyuktha05@gmail.com>
2024-02-19 08:00:07 +0530

    New Main added

--- Samples/Sample1.cpp
+++ Samples/Sample1.cpp
@@ -2,17 +2,7 @@
 
 using namespace std;
 
-int add(int x,int y)
+float main()
 {
-    return x+y;
-}
-
-int sub(int x, int y)
-{
-    return x-y;
-}
-int main()
-{
-   sub(10,20);
-    return 0;
+    return 0.0f;
 }
