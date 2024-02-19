241266e61fe63ec236270c190ffd573c410b4410
samyu05 <sheelamsamyuktha05@gmail.com>
2024-02-19 07:51:04 +0530

    Both changes from branch2 accepted

--- Samples/Sample1.cpp
+++ Samples/Sample1.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
+
 using namespace std;
+
+int add(int x,int y)
+{
+    return x+y;
+}
+
 int main()
 {
     cout<<"hi sadist Marvel"<<endl;
+    cout<<"some new applications designed"<<endl;
+    cout<<add(10,20)<<endl;
     return 0;
 }
